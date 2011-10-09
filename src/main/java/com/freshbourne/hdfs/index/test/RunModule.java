8191627fb1f343f1ce3f4efc10e2b65b11b3f782
Robin Wenglewski <robin@wenglewski.de>
2011-10-09 17:14:25 +0200

    different Guice Module API

--- src/main/java/com/freshbourne/hdfs/index/test/RunModule.java
+++ src/main/java/com/freshbourne/hdfs/index/test/RunModule.java
@@ -3,27 +3,12 @@ package com.freshbourne.hdfs.index.test;
 import com.freshbourne.hdfs.index.CSVModule;
 import com.freshbourne.hdfs.index.Index;
 import com.freshbourne.hdfs.index.IntegerCSVIndex;
+import com.google.inject.AbstractModule;
+
+import java.io.Serializable;
 
 public class RunModule extends CSVModule {
-
-
-    @java.lang.Override protected java.lang.String hdfsFile() {
-        return "/test/lineitem.tbl";
-    }
-
-    @java.lang.Override protected java.lang.String indexRootFolder() {
-        return "/tmp/index";
-    }
-
-    @java.lang.Override protected int csvColumn() {
-        return 0;
-    }
-
-    @java.lang.Override protected java.lang.String delimiter() {
-        return "|";
-    }
-
-	@Override protected Class<? extends Index> indexClass() {
-		return IntegerCSVIndex.class;
+	public RunModule(){
+		this.delimiter = "\\|";
 	}
 }
