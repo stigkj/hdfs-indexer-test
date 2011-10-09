b520a9b576c795cee704cecf56615102a1d51127
Robin Wenglewski <robin@wenglewski.de>
2011-10-09 11:19:46 +0200

    fix runJar

--- scripts/runJar.sh
+++ scripts/runJar.sh
@@ -8,9 +8,12 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-# used * instead of the concrete name to 1. be version independent and 2. so that also jars generated in the
-# vagrant env work (they are generated as vagrant.*.jar)
-hadoop jar build/libs/*.jar com.freshbourne.hdfs.index.test.Main "${SAMPLE_FILE_HDFS}"
+# use first jar in the libs directory and hope that it is the right one
+JAR=`ls build/libs/*.jar -1t|head -n 1`
+USE_INDEX=true
+JAVA_MAX=512
+hadoop jar ${JAR} com.freshbourne.hdfs.index.test.Main "${SAMPLE_FILE_HDFS}" ${USE_INDEX} -Dmapred.child.java.opts=-Xmx${JAVA_MAX}M
+
 
 # make sure indexer was successfull
 if [ $? -ne 0 ];then
