06949ae07b07c662e33088ce4dbf71296d7cf8b5
Robin Wenglewski <robin@wenglewski.de>
2011-10-15 10:23:35 +0200

    adjust RunModule to use new StringSerializer

--- src/main/java/com/freshbourne/hdfs/index/test/RunModule.java
+++ src/main/java/com/freshbourne/hdfs/index/test/RunModule.java
@@ -4,13 +4,24 @@ import com.freshbourne.btree.Range;
 import com.freshbourne.hdfs.index.CSVModule;
 import com.freshbourne.hdfs.index.Index;
 import com.freshbourne.hdfs.index.IntegerCSVIndex;
+import com.freshbourne.serializer.FixLengthSerializer;
+import com.freshbourne.serializer.StringCutSerializer;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.util.Modules;
 
 import java.io.Serializable;
 
-public class RunModule extends CSVModule {
-	public RunModule(){
-		this.delimiter = "\\|";
-		this.searchRange.add(new Range<Integer>(0, 10));
+public class RunModule extends AbstractModule implements Serializable {
+	
+	@Override protected void configure() {
+		CSVModule module = new CSVModule();
+		module.delimiter = "\\|";
+		module.searchRange.add(new Range<Integer>(0, 10));
+		install(Modules.override(module).with(new AbstractModule() {
+			@Override protected void configure() {
+				bind(new TypeLiteral<FixLengthSerializer<String, byte[]>>(){}).toInstance(StringCutSerializer.get(100));
+			}
+		}));
 	}
 }
